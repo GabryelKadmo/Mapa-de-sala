cc2ca419c4d0de34dd46ce044daa1ea5af66fc60
GabryelKadmo <gabryelkadmo@gmail.com>
2024-09-02 19:39:03 -0300

    fix: removed instagram

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,6 @@ export default function Footer() {
       <div className="footer-content">
         <p className="footer-text">© 2024 - Gabryel Kadmo | Gerador de Mapa de Sala</p>
         <ul className="footer-links">
-          <li><Link to="https://www.instagram.com/kxdmx.bat/" target="_blank" rel="noopener noreferrer">Instagram</Link></li>
           <li><Link to="https://github.com/GabryelKadmo" target="_blank" rel="noopener noreferrer">Github</Link></li>
           <li><Link to="https://www.linkedin.com/in/gabryel-kadmo/" target="_blank" rel="noopener noreferrer">LinkedIn</Link></li>
         </ul>
